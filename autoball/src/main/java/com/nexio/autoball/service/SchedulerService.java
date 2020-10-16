92c3257f2f7cd3207dae680f5cb9a48a5cfb2e97
sammy <sammy.chung@nexiosoft.com>
2020-10-16 16:56:36 +0800

    更新API

--- autoball/src/main/java/com/nexio/autoball/service/SchedulerService.java
+++ autoball/src/main/java/com/nexio/autoball/service/SchedulerService.java
@@ -1,6 +1,11 @@
 package com.nexio.autoball.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nexio.autoball.component.SocketClient;
+import com.nexio.autoball.model.BallCode;
+import com.nexio.autoball.model.BsArray;
+import com.nexio.autoball.model.GameInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +15,8 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @EnableAsync
@@ -24,7 +31,7 @@ public class SchedulerService {
 
     //每隔5秒执行一次
     @Async
-    @Scheduled(fixedRate = 5000)
+    @Scheduled(fixedRate = 120000)
     public void testTasks() {
         try {
             StringBuilder sb = new StringBuilder();
@@ -32,6 +39,7 @@ public class SchedulerService {
 
             String json = socketClient.send(sb.toString());
             logger.info("Run in testTasks {}", json);
+//            showGameInfo(json);
 
             CUR_GAME_NUM++;
         } catch (IOException e) {
@@ -41,19 +49,37 @@ public class SchedulerService {
     }
 
     //0,20,40 每20分執行一次
-    @Async
-    @Scheduled(cron = "0 0,20,40 * * * *")
-    public void testTasks2() {
-        try {
-            StringBuilder sb = new StringBuilder();
-            sb.append("startGame").append(",").append(CUR_GAME_NUM).append(",").append(GAME_COUNT).append(",").append(TIME_SPAM);
+//    @Async
+//    @Scheduled(cron = "0 0,20,40 * * * *")
+//    public void testTasks2() {
+//        try {
+//            StringBuilder sb = new StringBuilder();
+//            sb.append("startGame").append(",").append(CUR_GAME_NUM).append(",").append(GAME_COUNT).append(",").append(TIME_SPAM);
+//
+//            String json = socketClient.send(sb.toString());
+//            logger.info("Run in testTasks2 {}", json);
+//
+//            CUR_GAME_NUM++;
+//        } catch (IOException e) {
+//            logger.error("Task error", e);
+//        }
+//    }
 
-            String json = socketClient.send(sb.toString());
-            logger.info("Run in testTasks2 {}", json);
 
-            CUR_GAME_NUM++;
-        } catch (IOException e) {
-            logger.error("Task error", e);
+    private void showGameInfo(String json) throws JsonProcessingException {
+        ObjectMapper objectMapper =  new ObjectMapper();
+        GameInfo g = objectMapper.readValue(json,GameInfo.class);
+        ArrayList<String> list = new ArrayList<>();
+        for(BsArray bsarry:g.bsArray){
+            for(BallCode ballCode:bsarry.ballCode){
+                StringBuilder sb = new StringBuilder();
+                for (String ball:ballCode.code){
+                    sb.append(ball);
+                }
+                list.add(sb.toString());
+            }
         }
+        logger.info("ball={}", list);
+
     }
 }
--- autoball/src/main/java/com/nexio/sunzing/Server.java
+++ autoball/src/main/java/com/nexio/sunzing/Server.java
@@ -1,8 +1,6 @@
 package com.nexio.sunzing;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.nexio.autoball.model.BallCode;
-import com.nexio.autoball.model.BsArray;
 import com.nexio.autoball.model.GameInfo;
 
 import java.net.*;
@@ -16,6 +14,32 @@ public class Server {
     private DataInputStream inputStream;
     private Date date;
     private String inputMessage;
+
+    static String s0 ="{\"nGameNum\":100,\"dwGameTime\":1602821747,\"bsArray\":[{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"Q\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"Q\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"J\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"0\",\"0\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"4\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"3\"]}]}]}\n";
+    static String s1 ="{\"nGameNum\":101,\"dwGameTime\":1602821807,\"bsArray\":[{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"8\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"7\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"J\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"4\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"4\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"3\"]}]}]}";
+    static String s2 ="{\"nGameNum\":102,\"dwGameTime\":1602821867,\"bsArray\":[{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"0\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"Q\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"0\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"4\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"J\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"0\"]}]}]}";
+    static String s3 ="{\"nGameNum\":103,\"dwGameTime\":1602821962,\"bsArray\":[{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"J\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"0\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"Q\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"0\",\"0\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"A\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"5\"]}]}]}\n";
+    static String s4 ="{\"nGameNum\":104,\"dwGameTime\":1602822022,\"bsArray\":[{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"J\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"J\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"Q\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"5\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"Q\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"A\",\"B\",\"C\",\"D\"]}]}]}\n";
+    static String s5 ="{\"nGameNum\":105,\"dwGameTime\":1602822082,\"bsArray\":[{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"1\",\"0\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"0\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"A\",\"B\",\"C\",\"D\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"5\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"1\",\"0\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"3\"]}]}]}";
+    static String s6 ="{\"nGameNum\":106,\"dwGameTime\":1602822142,\"bsArray\":[{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"K\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"A\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"A\",\"B\",\"C\",\"D\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"A\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"J\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"K\"]}]}]}";
+    static String s7 ="{\"nGameNum\":107,\"dwGameTime\":1602822202,\"bsArray\":[{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"1\",\"0\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"0\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"0\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"A\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"1\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"0\"]}]}]}";
+    static String s8 ="{\"nGameNum\":108,\"dwGameTime\":1602822262,\"bsArray\":[{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"0\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"4\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"J\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"5\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"7\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"A\",\"B\",\"C\",\"D\"]}]}]}";
+    static String s9 ="{\"nGameNum\":109,\"dwGameTime\":1602822436,\"bsArray\":[{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"8\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"7\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"0\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"A\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"K\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"5\"]}]}]}";
+    static String s10 ="{\"nGameNum\":110,\"dwGameTime\":1602822838,\"bsArray\":[{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"J\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"4\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"A\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"9\",\"W\",\"X\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"2\"]}]},{\"nBallCount\":1,\"BallCode\":[{\"code\":[\"5\"]}]}]}";
+    static ArrayList<String> list = new ArrayList<>();
+    static{
+        list.add(s0);
+        list.add(s1);
+        list.add(s2);
+        list.add(s3);
+        list.add(s4);
+        list.add(s5);
+        list.add(s6);
+        list.add(s7);
+        list.add(s8);
+        list.add(s9);
+        list.add(s10);
+    }
 
     public Server() {
         try {
@@ -49,75 +73,22 @@ public class Server {
         }
     }
 
-    private String testData(int nGameNum) throws JsonProcessingException {
-        GameInfo gameInfo = new GameInfo();
-        gameInfo.nGameNum = nGameNum;
-        gameInfo.dwGameTime = System.currentTimeMillis();
-
-
-        BsArray bsArray1 = new BsArray();
-        BallCode ballCode11 = new BallCode();
-        ballCode11.code = new ArrayList<>();
-        ballCode11.code.add("1");
-        bsArray1.nBallCount= 1;
-        bsArray1.ballCode.add(ballCode11);
-
-        BsArray bsArray2 = new BsArray();
-        BallCode ballCode21 = new BallCode();
-        ballCode21.code = new ArrayList<>();
-        ballCode21.code.add("2");
-        bsArray2.nBallCount= 1;
-        bsArray2.ballCode.add(ballCode21);
-
-        BsArray bsArray3 = new BsArray();
-        BallCode ballCode31 = new BallCode();
-        ballCode31.code = new ArrayList<>();
-        ballCode31.code.add("3");
-        bsArray3.nBallCount= 1;
-        bsArray3.ballCode.add(ballCode31);
-
-        BsArray bsArray4 = new BsArray();
-        BallCode ballCode41 = new BallCode();
-        ballCode41.code = new ArrayList<>();
-        ballCode41.code.add("4");
-        bsArray4.nBallCount= 1;
-        bsArray4.ballCode.add(ballCode41);
-
-        BsArray bsArray5 = new BsArray();
-        BallCode ballCode51 = new BallCode();
-        ballCode51.code = new ArrayList<>();
-        ballCode51.code.add("5");
-        bsArray5.nBallCount= 1;
-        bsArray5.ballCode.add(ballCode51);
-
-        BsArray bsArray6 = new BsArray();
-        BallCode ballCode61 = new BallCode();
-        ballCode61.code = new ArrayList<>();
-        ballCode61.code.add("6");
-        bsArray6.nBallCount= 2;
-
-        BallCode ballCode62 = new BallCode();
-        ballCode62.code = new ArrayList<>();
-        ballCode62.code.add("7");
-
-        bsArray6.ballCode.add(ballCode61);
-        bsArray6.ballCode.add(ballCode62);
-
-        gameInfo.bsArray = new ArrayList<>();
-        gameInfo.bsArray.add(bsArray1);
-        gameInfo.bsArray.add(bsArray2);
-        gameInfo.bsArray.add(bsArray3);
-        gameInfo.bsArray.add(bsArray4);
-        gameInfo.bsArray.add(bsArray5);
-        gameInfo.bsArray.add(bsArray6);
+    public static String testData(int nGameNum) throws JsonProcessingException {
+        Random random = new Random();
+        int mod = random.nextInt(10);
+        String src = list.get(mod);
 
         ObjectMapper objectMapper =  new ObjectMapper();
-        String json = objectMapper.writeValueAsString(gameInfo);
 
+        GameInfo g = objectMapper.readValue(src,GameInfo.class);
+        g.nGameNum = nGameNum;
+
+        String json = objectMapper.writeValueAsString(g);
         return json;
     }
 
-    public static void main(String args[]) {
+    public static void main(String args[]) throws JsonProcessingException {
+//        Server.testData(1);
         new Server();
     }
 }
