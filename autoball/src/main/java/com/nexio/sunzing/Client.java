7de300c9ba2ec26bcdfc3e1730b7d3490227efb3
ChengYin <habit.cas@msa.hinet.net>
2020-10-16 17:32:03 +0800

    push zip

--- autoball/src/main/java/com/nexio/sunzing/Client.java
+++ autoball/src/main/java/com/nexio/sunzing/Client.java
@@ -13,12 +13,16 @@ public class Client {
         try(Socket socket =  new Socket(InetAddress.getByName("127.0.0.1"), 5566);
             DataInputStream input = new DataInputStream(socket.getInputStream());
             DataOutputStream output = new DataOutputStream(socket.getOutputStream());) {
-            output.writeUTF("startGame:108,1,0");
-            String inputMessage = input.readUTF();
+            String s = "startGame,108,1,0";
+            output.write(s.getBytes(),0,s.getBytes().length);
+
+            byte[] bytes = new byte[1024];
+            input.read(bytes);
+            String inputMessage = new String(bytes);
             System.out.println("Message from Server: " + inputMessage);
         }
-        catch(IOException ioException) {
-            ioException.printStackTrace();
+        catch(Throwable e) {
+            e.printStackTrace();
         }
     }
     public static void main(String args[]) {
