46091e7fe0609f1f6c1081d50f9111b6ad3d3804
sammy <sammy.chung@nexiosoft.com>
2020-10-19 14:50:50 +0800

    更新API

--- autoball/src/main/java/com/nexio/autoball/service/DrawService.java
+++ autoball/src/main/java/com/nexio/autoball/service/DrawService.java
@@ -1,13 +1,18 @@
 package com.nexio.autoball.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
 @Service
 public class DrawService {
+    private static final Logger logger = LoggerFactory.getLogger(DrawService.class);
 
     public String draw(String gameNum, String drawNums) {
+
+        logger.info("GameNum[{}] drawNums[{}]",gameNum,drawNums);
         return "";
     }
 
